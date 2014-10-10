9f1ce499d8993fe80196b55352e5d2197f268aa7
Scott So <scotthhso@gmail.com>
2014-10-10 01:13:53 -0400

    Modified first screen

---
+++ app/src/main/java/scottso/assist911/ThirdTutorialFragment.java
@@ -0,0 +1,20 @@
+package scottso.assist911;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by scottso on 2014-09-08.
+ */
+public class ThirdTutorialFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_secondtutorial, container, false);
+    }
+
+
+}
